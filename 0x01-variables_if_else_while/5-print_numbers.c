87b9ecfcb318aa6610825c276de411d9517ae9ee
DanteDiagramma <sethgeorge20@gmail.com>
2022-07-04 01:35:37 -0700

    numbers 0-9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 Success
+ */
+int main(void)
+{
+	int numbers;
+
+	for (number = 0; numbers < 10; numbers++)
+		printf("%d", numbers);
+	printf("\n")
+	return (0);
+}
