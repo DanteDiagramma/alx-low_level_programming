3abe89ac666a4ae61536d6d0b78a0275651f30ee
DanteDiagramma <sethgeorge20@gmail.com>
2022-07-06 16:38:00 -0700

    maximum effort

--- 0x02-functions_nested_loops/3-main.c
+++ 0x02-functions_nested_loops/3-main.c
@@ -9,12 +9,12 @@ int main(void)
 {
 	int r;
 	
-	r = islower('H');
-	putchar(r + '0');
-	r = islower('o');
-	putchar(r + '0');
+	r = _islower('H');
+	_putchar(r + '0');
+	r = _islower('o');
+	_putchar(r + '0');
 	r = _islower(108);
-	putchar(r + '0');
-	putchar('\n');
+	_putchar(r + '0');
+	_putchar('\n');
 	return (0);
 }
