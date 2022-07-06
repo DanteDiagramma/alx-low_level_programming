5b65f91e3576bccc4b1d89317230852dde03fdec
DanteDiagramma <sethgeorge20@gmail.com>
2022-07-06 02:46:09 -0700

    lower case alphs

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * print_alphabet - a function that prints out the alphabets in lowercase
+ *
+ * Return: Always 0 Success
+ */
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar (i);
+	_putchar ('\n');
+	}
+}
