f3ed2085a609fb9933e5073a895d34869869b9b6
DanteDiagramma <sethgeorge20@gmail.com>
2022-07-06 07:31:56 -0700

    10 alphs

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<main.h>
 /**
  * print_alphabet_x10 - prints the alphabets in 10 lines
  *
@@ -9,11 +10,12 @@ void print_alphabet_x10(void)
 {
 	int j = 0;
 	char i;
-
-	while (j < 10)
-		for(i = 'a'; i <= 'z'; i++)
+	
+	while(j < 10)
+	{
+		for (i = 'a'; i <= 'z'; i++)
 			putchar(i);
 		putchar('\n');
 		j++;
-		
+	}	
 }
